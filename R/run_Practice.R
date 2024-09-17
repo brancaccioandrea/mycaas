b455eb30f2fddeb46bce49ef0724971b975290d1
Andrea Brancaccio <andreabrancaccio01@gmail.com>
2024-09-17 13:39:44 +0200

    Package Update

---
+++ R/run_Practice.R
@@ -0,0 +1,18 @@
+#' run Practice Assessment shinyApp
+#'
+#' This function run a shinyApp in which the following pages are available:
+#' i) a brief introduction of the assessment tools; ii) the GUI to create "assessment" files from a "csv" of items;
+#' iii) the GUI implementation of the adaptive assessment; and iv) the GUI implementation of the outcome.
+#' @return The results of the assessment
+#' @export
+run_Practice <- function() {
+  if(system.file(package='shiny')=="")
+  {
+    stop("The shiny package is required to run this function.")
+  }
+  appDir <- system.file("App","App.R", package = "AAT")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `AAT`.", call. = FALSE)
+  }
+  shiny::runApp(appDir = appDir)
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(AAT)
+
+test_check("AAT")
